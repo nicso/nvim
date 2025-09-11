9f06e1783d9a5f21dc7c88344b4a7bce71683fb0
YannickSala <y.sala@alltechconsulting.fr>
2025-09-11 18:39:42 +0200

    fixed snippets

--- lua/plugins/blink_cmp.lua
+++ lua/plugins/blink_cmp.lua
@@ -2,36 +2,44 @@ vim.g.ai_cmp = false
 
 return {
   "Saghen/blink.cmp",
-  dependencies = { "echasnovski/mini.snippets" },
+  dependencies = { 
+    "L3MON4D3/LuaSnip",
+    "rafamadriz/friendly-snippets"
+  },
   enabled = true,
   event = "InsertEnter",
-  config = function()
-    -- Initialize mini.snippets
-    require("mini.snippets").setup()
-    
-    require("blink.cmp").setup({
-      keymap = {
-        preset = "default",
-        ["<Tab>"] = { "accept", "fallback" },
-        ["<S-Tab>"] = { "select_prev", "fallback" },
-        ["<C-l>"] = { "accept", "fallback" },
-        ["<C-c>"] = { "cancel", "fallback" },
-        ["<C-space>"] = {
-          function(cmp)
-            cmp.show({ providers = { "snippets" } })
-          end,
-        },
+  opts = {
+    snippets = { 
+      preset = "luasnip",
+    },
+    sources = {
+      default = { "snippets", "lsp", "path", "buffer" },
+    },
+    keymap = {
+      preset = "default",
+      ["<Tab>"] = { "accept", "fallback" },
+      ["<S-Tab>"] = { "select_prev", "fallback" },
+      ["<C-l>"] = { "accept", "fallback" },
+      ["<C-c>"] = { "cancel", "fallback" },
+      ["<C-space>"] = {
+        function(cmp)
+          cmp.show({ providers = { "snippets" } })
+        end,
       },
-    })
-  end,
-  opts = function(_, opts)
-    -- ✅ Configuration simplifiée et corrigée
+    },
+  },
+  config = function(_, opts)
+    local luasnip = require("luasnip")
+    
+    -- Load friendly-snippets
+    require("luasnip.loaders.from_vscode").lazy_load()
+    
+    -- Make TypeScript use JavaScript snippets too
+    luasnip.filetype_extend("typescript", { "javascript" })
+    luasnip.filetype_extend("typescriptreact", { "javascript", "typescript" })
+    
+    -- Setup completion toggle
     local completion_enabled = true
-    opts.snippets = { preset = "mini_snippets" }
-    opts.sources = {
-      default = { "lsp", "path", "snippets", "buffer" },
-    }
-    -- Toggle pour la completion
     vim.keymap.set("n", "<leader>uk", function()
       completion_enabled = not completion_enabled
       vim.b.completion = completion_enabled
@@ -41,7 +49,7 @@ return {
     opts.enabled = function()
       return vim.b.completion ~= false and completion_enabled
     end
-
-    return opts
+    
+    require("blink.cmp").setup(opts)
   end,
 }
