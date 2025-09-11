8b56a9b5530379c5538388c9c9637266a85a0f1e
YannickSala <y.sala@alltechconsulting.fr>
2025-09-11 18:42:50 +0200

    fixed tabulation to 2 characters

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,7 +5,7 @@ vim.opt.relativenumber = true
 
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 4
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
